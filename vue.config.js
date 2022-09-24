22c116917a4b5362805b3d94d91b534d15c2576e
OleinikovaPolina <oleinikowa.poly@gmail.com>
2022-09-24 20:17:14 +0300

    public 2

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   publicPath: process.env.NODE_ENV === 'production'
-    ? 'https://github.com/OleinikovaPolina/portfolio/dist/'
+    ? 'https://oleinikovapolina.github.io/portfolio/dist/'
     : '/',
   transpileDependencies: true,
   pluginOptions: {
